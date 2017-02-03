21082b64e5cec51d325499e4b49b618ad3ebd8b7
Mayur Agrawal <magrawal@movideo.com>
2017-02-03 16:29:20 +0530

    created lib

--- app/src/main/java/com/mayur_agrawal/maventestlib/MainActivity.java
+++ app/src/main/java/com/mayur_agrawal/maventestlib/MainActivity.java
@@ -1,13 +1,4 @@
 package com.mayur_agrawal.maventestlib;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+public class MainActivity {
 }
